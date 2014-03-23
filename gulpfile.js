ce0e3a9347ad2b6ae2d62e9ba40cc65dab5d4198
SirReal <jon@surrell.es>
2014-03-23 11:14:55 +0100

    redo, from the ground up

---
+++ gulpfile.js
@@ -0,0 +1,100 @@
+var gulp = require('gulp'),
+    gutil = require('gulp-util'),
+    concat = require('gulp-concat'),
+    jscs = require('gulp-jscs'),
+    jshint = require('gulp-jshint'),
+    less = require('gulp-less'),
+    uglify = require('gulp-uglify')
+
+    // Non gulp
+var express = require('express'),
+    spawn = require('child_process').spawn
+
+    // Vars
+var paths = {
+      site: '_site',
+      lessIn: '_dev/less/main.less',
+      lessOut: 'css',
+      jsIn: [
+        '_dev/js/main.js',
+      ],
+      jsOut: 'js',
+    }
+
+
+var jekyll = function(jekyllParams) {
+  return spawn('jekyll', jekyllParams).on('exit', function(e) {
+    if (e > 0)
+    {
+      gutil.log('Jekyll error. Is jekyll installed?')
+      gutil.log('`gem install jekyll`')
+      throw new gutil.PluginError('jekyll', 'Jekyll error :(')
+    }
+  })
+}
+
+
+gulp.task('css', function() {
+  return gulp.src(paths.lessIn)
+    .pipe(less({
+      paths: '',
+      // compress: true,
+      strictMath: true,
+      cleanCss: true
+    }).on('error', gutil.log))
+    .pipe(gulp.dest(paths.lessOut))
+})
+
+gulp.task('js', function() {
+  return gulp.src(paths.jsIn)
+    .pipe(uglify({
+      preserveComments: 'some'
+    }))
+    .pipe(gulp.dest(paths.jsOut))
+})
+
+gulp.task('jekyll-build', ['css', 'js'], function() {
+  jekyll(['build'])
+})
+
+gulp.task('default', ['css', 'js'], function() {
+  var lrPort = 35729
+
+  var j = jekyll(['build', '--watch', '--drafts'])
+  j.stdout
+    .on('readable', function() {
+      var output = this.read().toString().trim()
+      gutil.log('JEKYLL: ' + output)
+      if (output === '...done.')
+      {
+        lr.changed({
+          body: {
+            files: [
+              '*'
+            ]
+          }
+        })
+      }
+    })
+
+  // Jekyll doesn't seem to produce stderr
+  j.stderr
+    .on('readable', function() {
+      gutil.log(this.read().toString().trim())
+    })
+    .on('end', function () {
+      throw new gutil.PluginError('Jekyll error.')
+    })
+
+  express()
+    .use(require('connect-livereload')(lrPort))
+    .use(express.static('_site'))
+    .listen(4000)
+
+  var lr = require('tiny-lr')()
+  lr.listen(lrPort)
+
+  gulp.watch('_dev/less/**/*.{less,css}', ['css'])
+  gulp.watch(paths.jsIn, ['js'])
+
+})
