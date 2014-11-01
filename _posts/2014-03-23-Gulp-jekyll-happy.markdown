d34a71bdec1eca1acd5c0ba75617dd965309fbbe
SirReal <jon.surrell@gmail.com>
2014-11-01 23:08:19 +0100

    working towards gulp build

---
+++ _posts/2014-03-23-Gulp-jekyll-happy.markdown
@@ -0,0 +1,23 @@
+---
+layout: post
+title: 'Gulp, jekyll, Github Pages: building this site.'
+---
+Over the course of the past year or so I have had the pleasure of adopting many of the new tools available to web developers.
+
+This all started thanks to [Boostrap](http://getbootstrap.com/). I'd been interested in css preprocessors for a while, and, after diving in thanks to Bootstrap, I started using [Less](http://lesscss.org/) instead of vanilla CSS whenever possible, AKA always. That was great, because Less made my life better in general. Everything was good. I though I was really rocking. I even got really hyped about `make` and Makefiles and all that cool stuff, which I'd never really thought of applying to my workflow.
+
+Then Bootstrap started using [Grunt](http://gruntjs.com/), which was another one of those things I'd been hearing about but hadn't really gotten my feet wet. All of a sudden that pretty ugly, hacky feeling Makefile had turned into some that seemed a lot better. Everything just seemed clearer and like "this was it". It was like make 2.0.
+
+<!-- maybe get into the less/sass/stylus... stuff. How it's all good, bla bla -->
+
+Then all of a sudden twitter seemed to blow up and the dev community collectively decided Grunt was soooo-last-week and Gulp was *IT*. Naturally I wanted to catch this trend on it's upswing and not wait until it was already in Bootstrap, so I started playing with it a bunch, got pretty frustrate along the way, and learned a bunch.
+
+Mark Dahls article was interesting, it's not as mature at Grunt. but it's fun to play with
+
+those other gulp articles were great.
+
+Right now, as I write this article, I've got a solid setup. I plan to make some improvements, there are a few things I know I want to add, but right now I'm pretty darn happy and I'd like to share that with you.
+
+1. requirements
+1. gulp requires
+1.
--- gulpfile.js
+++ gulpfile.js
@@ -4,67 +4,55 @@ var gulp = require('gulp'),
     jscs = require('gulp-jscs'),
     jshint = require('gulp-jshint'),
     less = require('gulp-less'),
-    uglify = require('gulp-uglify')
+    uglify = require('gulp-uglify'),
+    jade = require('gulp-jade')
 
-// Non gulp
-var express = require('express'),
-    spawn = require('child_process').spawn
+var express = require('express')
 
-// Vars
 var paths = {
       site: '_site',
-      lessIn: '_dev/less/main.less',
-      lessOut: 'css',
-      jsIn: [
-        '_dev/js/main.js',
-      ],
-      jsOut: 'js',
+      posts: '_posts/**/*.jade',
+      layouts: '_layouts/**/*.jade',
+      less: {
+        in: '_dev/less/main.less',
+        out: 'css'
+      },
+      js: {
+        in: [
+          '_dev/js/main.js',
+        ],
+        out: 'js'
+      }
     }
 
-
-var jekyll = function(jekyllParams) {
-  return spawn('jekyll', jekyllParams).on('exit', function(e) {
-    if (e > 0)
-    {
-      gutil.log('Jekyll error. Is jekyll installed?')
-      gutil.log('`gem install jekyll`')
-      throw new gutil.PluginError('jekyll', 'Jekyll error :(')
-    }
-  })
-}
 
 gulp.task('css', function() {
-  return gulp.src(paths.lessIn)
+  return gulp.src(paths.less.in)
     .pipe(less({
       paths: '',
       // compress: true,
       strictMath: true,
       cleanCss: true
     }).on('error', gutil.log))
-    .pipe(gulp.dest(paths.lessOut))
+    .pipe(gulp.dest(paths.less.out))
 })
 
+
 gulp.task('js', function() {
-  return gulp.src(paths.jsIn)
+  return gulp.src(paths.js.in)
     .pipe(uglify({
       preserveComments: 'some'
     }))
-    .pipe(gulp.dest(paths.jsOut))
+    .pipe(gulp.dest(paths.js.out))
 })
 
-/**
-* Production builds
-*/
-gulp.task('build', ['css', 'js'], function() {
-  return jekyll(['build'])
-})
 
-/**
-* Dev builds
-* includes drafts & future
-*/
-gulp.task('dev', ['css', 'js'], function() {
-  return jekyll(['build', '--drafts'])
+gulp.task('build', function() {
+  // var tpl = jade.compileFile(templates.post)
+
+  return gulp.src(paths.posts)
+    .pipe(jade())
+    .pipe(gulp.dest(paths.site))
 })
 
 
@@ -75,52 +63,23 @@ gulp.task('dev', ['css', 'js'], function() {
 *  [ ] js
 *  [ ] css
 */
-gulp.task('test', ['build'], function() {
-})
+gulp.task('test', ['build'], function() {})
 
 /**
 * Default task
 * build & watch
 * used for local dev
 */
-gulp.task('default', ['css', 'js'], function() {
+gulp.task('default', ['css', 'js', 'build'], function() {
   var lrPort = 35729
-
-  var j = jekyll(['build', '--watch', '--drafts', '--future'])
-  j.stdout
-    .on('readable', function() {
-      var output = this.read().toString().trim()
-      gutil.log('JEKYLL: ' + output)
-      if (output === '...done.')
-      {
-        lr.changed({
-          body: {
-            files: [
-              '*'
-            ]
-          }
-        })
-      }
-    })
-
-  // Jekyll doesn't seem to produce stderr
-  j.stderr
-    .on('readable', function() {
-      gutil.log(this.read().toString().trim())
-    })
-    .on('end', function () {
-      throw new gutil.PluginError('Jekyll error.')
-    })
+  var lr = require('tiny-lr')()
+  lr.listen(lrPort)
 
   express()
     .use(require('connect-livereload')(lrPort))
     .use(express.static('_site'))
     .listen(4000)
 
-  var lr = require('tiny-lr')()
-  lr.listen(lrPort)
-
-  gulp.watch('_dev/less/**/*.{less,css}', ['css'])
-  gulp.watch(paths.jsIn, ['js'])
-
+  gulp.watch(paths.js.in, ['js'])
+  gulp.watch([paths.posts, paths.layouts], ['build'])
 })
