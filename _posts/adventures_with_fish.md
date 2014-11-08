3c135cadec0e2a34d92d4764035f405d4dcbe271
sirreal <jon.surrell@gmail.com>
2014-11-08 20:55:22 +0100

    jade + markdown posts

---
+++ _posts/adventures_with_fish.md
@@ -0,0 +1,31 @@
+# Adventures with Fish (shell)
+
+<pre class="pull-right">
+                 ___
+  ___======____=---=)
+/T            \_--===)
+L \ (@)   \~    \_-==)
+ \      / )J~~    \-=)
+  \\___/  )JJ~~    \)
+   \_____/JJJ~~      \
+   / \  , \J~~~~      \
+  (-\)\=|  \~~~        L__
+  (\\)  ( -\)_            ==__
+   \V    \-\) ===_____  J\   \\
+          \V)     \_) \   JJ J\)
+                      /J JT\JJJJ)
+                      (JJJ| \UUU)
+                       (UU)
+</pre>
+
+Over the past couple of weeks, I've been [fish][fish], the Friendly Interactive SHell, as my primary shell. I spend all of my working day and much of my free time in a terminal. The experience has been largely satisfying, I can't say I've found any _real_ turnoffs at this point,
+
+## TL;DR
+
+
+
+1. Is familiar (or close enough to bash that there's no big learning curve).
+1. Is fast.
+1. Has excellent completion support.
+
+[fish]: http://fishshell.com
--- gulpfile.js
+++ gulpfile.js
@@ -1,34 +1,51 @@
+// jshint node:true
+'use strict';
 var gulp = require('gulp'),
     gutil = require('gulp-util'),
     concat = require('gulp-concat'),
+    gjade = require('gulp-jade'),
+    jade = require('jade'),
     jscs = require('gulp-jscs'),
     jshint = require('gulp-jshint'),
     less = require('gulp-less'),
-    uglify = require('gulp-uglify'),
-    jade = require('gulp-jade');
+    markdown = require('gulp-markdown'),
+    through = require('through2'),
+    uglify = require('gulp-uglify');
 
 var express = require('express');
 
 var paths = {
-      site: '_site',
-      blog: '_site/blog',
-      posts: '_posts/**/*.jade',
-      pages: '_pages/**/*.jade',
-      static: '_static/**/*',
-      layouts: '_layouts/**/*.jade',
-      includes: '_includes/**/*.jade',
-      less: {
-        in: '_dev/less/main.less',
-        out: 'css'
-      },
-      js: {
-        in: [
-          '_dev/js/main.js',
-        ],
-        out: 'js'
-      }
-    };
+  site: '_site',
+  blog: '_site/blog',
+  posts: '_posts/**/*.md',
+  pages: '_pages/**/*.jade',
+  static: '_static/**/*',
+  layouts: '_layouts/**/*.jade',
+  includes: '_includes/**/*.jade',
+  less: {
+    in: '_dev/less/main.less',
+    out: 'css'
+  },
+  js: {
+    in: [
+      '_dev/js/main.js',
+    ],
+    out: 'js'
+  }
+};
 
+function applyTemplate(templateFile) {
+  var tpl = jade.compileFile(templateFile);
+
+  return through.obj(function (file, enc, cb) {
+    var locals = {
+        content: file.contents.toString()
+    };
+    file.contents = new Buffer(tpl(locals), 'utf8');
+    this.push(file);
+    cb();
+  });
+}
 
 gulp.task('css', function() {
   return gulp.src(paths.less.in)
@@ -37,10 +54,9 @@ gulp.task('css', function() {
       // compress: true,
       strictMath: true,
       cleanCss: true
-    }).on('error', gutil.log))
+    }).on('error', gutil.log.bind(gutil)))
     .pipe(gulp.dest(paths.less.out));
 });
-
 
 gulp.task('js', function() {
   return gulp.src(paths.js.in)
@@ -50,28 +66,26 @@ gulp.task('js', function() {
     .pipe(gulp.dest(paths.js.out));
 });
 
-
 gulp.task('build', ['build:posts', 'build:pages', 'copy']);
-
-gulp.task('build:posts', function() {
-  return gulp.src(paths.posts)
-    .pipe(jade())
-      .on('error', function(e) {console.log(e);})
-    .pipe(gulp.dest(paths.blog));
-});
 
 gulp.task('build:pages', function() {
   return gulp.src(paths.pages)
-    .pipe(jade())
+    .pipe(gjade())
       .on('error', function(e) {console.log(e);})
     .pipe(gulp.dest(paths.site));
+});
+
+gulp.task('build:posts', function() {
+  return gulp.src(paths.posts)
+    .pipe(markdown({gfm: true}))
+    .pipe(applyTemplate('_layouts/post.jade'))
+    .pipe(gulp.dest(paths.blog));
 });
 
 gulp.task('copy', function() {
   return gulp.src(paths.static)
-    .pipe(gulp.dest(paths.site));
+    .pipe(gulp.dest(paths.site + '/static'));
 });
-
 
 /**
 * tests
@@ -97,6 +111,8 @@ gulp.task('default', ['css', 'js', 'build'], function() {
     .use(express.static('_site'))
     .listen(4000);
 
+  gutil.log('Live preview @ http://localhost:4000');
+
   gulp.watch(paths.js.in, ['js']);
   gulp.watch(paths.pages, function(e) {
     console.log(e);
@@ -109,6 +125,7 @@ gulp.task('default', ['css', 'js', 'build'], function() {
       }
     });
   });
+
   gulp.watch(paths.posts, function(e) {
     console.log(e);
     gulp.start('build:posts');
